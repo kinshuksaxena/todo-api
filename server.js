4024720c4812716a2814048fced5da2fc5ff47d6
Kinshuk Saxena <kinshuk.saxena@gmail.com>
2016-02-18 21:47:13 -0500

    Add POST to todos

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-	id: 1,
-	description: 'call the operator',
-	completed: false
-}, {
-	id: 2,
-	description: 'meet the VC',
-	completed: false
-}, {
-	id: 3,
-	description: 'Find more funding',
-	completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res){
 	res.send('Todo API root 1');
@@ -25,7 +18,7 @@ app.get('/todos', function (req, res){
 });
 
 //GET /todos/:id
-app.get('/todos/:id', function(req, res){
+app.get('/todos/:id', function (req, res){
 	var todoId = parseInt(req.params.id);
 	var matchTodo;
 
@@ -41,6 +34,18 @@ app.get('/todos/:id', function(req, res){
 		res.status(404).send();
 	};
 });
+
+// POST /todos
+app.post('/todos', function (req, res) {
+	var body = req.body;
+
+	body.id = todoNextId++;
+
+	todos.push(body);
+	
+	res.json(body);
+});
+
 
 app.listen(PORT, function () {
 	console.log('Express listening on port ' + PORT);
