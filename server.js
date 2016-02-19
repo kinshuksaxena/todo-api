771b29097eecf6c45117f45c9c9ac361057c1afc
Kinshuk Saxena <kinshuk.saxena@gmail.com>
2016-02-18 23:38:47 -0500

    Add PUT to todo

--- server.js
+++ server.js
@@ -72,6 +72,35 @@ app.delete('/todos/:id', function(req, res){
 
 })
 
+// PUT /todos/:id
+app.put('/todos/:id', function(req, res){
+	var todoId = parseInt(req.params.id);
+	var matchTodo = _.findWhere(todos, {id: todoId});
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttribute = {};
+
+	if (!matchTodo){
+		return res.status(404).send();
+	}
+
+
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+		validAttribute.completed = body.completed;
+	} else if (body.hasOwnProperty('completed')){
+		return res.status(400).send();
+	}; 
+
+	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0){
+		validAttribute.description = body.description;
+	} else if (body.hasOwnProperty('description')){
+		return res.status(400).send();
+	}; 	
+
+	_.extend(matchTodo, validAttribute);
+
+	res.json(matchTodo);
+})
+
 app.listen(PORT, function () {
 	console.log('Express listening on port ' + PORT);
 });
