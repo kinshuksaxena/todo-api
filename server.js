86aa7fd3eaf48f0cf4a5f1885359a6ab553317a8
Kinshuk Saxena <kinshuk.saxena@gmail.com>
2016-02-18 22:32:36 -0500

    Refactor API with underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -20,13 +21,14 @@ app.get('/todos', function (req, res){
 //GET /todos/:id
 app.get('/todos/:id', function (req, res){
 	var todoId = parseInt(req.params.id);
-	var matchTodo;
+	var matchTodo = _.findWhere(todos, {id: todoId});
+	// var matchTodo;
 
-	todos.forEach(function (todo){
-		if (todoId === todo.id){
-			matchTodo = todo;
-		}
-	});
+	// todos.forEach(function (todo){
+	// 	if (todoId === todo.id){
+	// 		matchTodo = todo;
+	// 	}
+	// });
 
 	if (matchTodo){
 		res.json(matchTodo);
@@ -37,7 +39,14 @@ app.get('/todos/:id', function (req, res){
 
 // POST /todos
 app.post('/todos', function (req, res) {
-	var body = req.body;
+	var body = _.pick(req.body, 'description', 'completed');
+
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+		return res.status(400).send();
+	}
+    
+    body.description = body.description.trim();
+
 
 	body.id = todoNextId++;
 
